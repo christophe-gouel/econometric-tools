2aed110dad6b61e7cc8a36aaaf890fc69b1b3700
Christophe Gouel <christophe.gouel@grignon.inra.fr>
2014-12-01 09:55:31 +0100

    Add the option Vectorized to numjac.m to speed-up calculations

--- numjac.m
+++ numjac.m
@@ -3,22 +3,23 @@ function J = numjac(FUN,x,options,varargin)
 %
 % J = NUMJAC(FUN,X) computes the Jacobian J by central finite differences
 % applied to function FUN (function name or function handle) that accepts a vector
-% X as input and return a vector F. The Jacobian is evaluated at X. 
+% X as input and return a vector F. The Jacobian is evaluated at X.
 %
 % J = NUMJAC(FUN,X,options) uses the structure OPTIONS to calculate the
 % Jacobian. The fields of the structure are
-%  FinDiffType    : 'central' (default) or 'forward' for central or forward 
-%                   finite differences
-%  FinDiffRelStep : scalar or vector step size factor. For central finite 
+%  FinDiffType    : {'central'} or 'forward' for central or forward finite
+%                   differences
+%  FinDiffRelStep : scalar or vector step size factor. For central finite
 %                   differences, the default value is FinDiffRelStep = eps^(1/3),
 %                   and for forward finite differences FinDiffRelStep = sqrt(eps).
 %  SerialProblem  : true or false (default), if true considers the problem as a
-%                   serial problem: X is a n-by-d matrix and FUN(X) outputs a 
-%                   n-by-d matrix, such that each line is independent from the 
-%                   other. In this case, NUMJAC returns a sparse block diagonal 
+%                   serial problem: X is a n-by-d matrix and FUN(X) outputs a
+%                   n-by-d matrix, such that each line is independent from the
+%                   other. In this case, NUMJAC returns a sparse block diagonal
 %                   nxd-by-nxd Jacobian.
+%  Vectorized     : 'on' or {'off'} to specify whether FUN is vectorized
 
-% Copyright (C) 2011-2013 Christophe Gouel
+% Copyright (C) 2011-2014 Christophe Gouel
 % Licensed under the Expat license, see LICENSE.txt
 
 %% Initialization
@@ -31,7 +32,8 @@ end
 defaultopt = struct(             ...
     'FinDiffType'    , 'central',...
     'FinDiffRelStep' , []       ,...
-    'SerialProblem'  , false);
+    'SerialProblem'  , false    ,...
+    'Vectorized'     , 'off');
 
 if nargin <= 2
   options = defaultopt;
@@ -54,36 +56,60 @@ end
 
 if isempty(epsilon)
   if central
-    epsilon = eps^(1/3); 
+    epsilon = eps^(1/3);
   else
-    epsilon = sqrt(eps); 
+    epsilon = sqrt(eps);
   end
 end
 
-step  = epsilon.*max(abs(x),1);
+x     = x(:);
+step  = epsilon(:).*max(abs(x),1);
 x1    = x+step;
 if central
   x0  = x-step;
-  h   = x1-x0; 
+  h   = x1-x0;
 else
   f0  = FUN(x,varargin{:});
   h   = x1-x;
 end
-  
+
 %% Computation of finite difference Jacobian
 if ~options.SerialProblem
-  %% Traditional problem
-  d         = length(x);
-  for j=1:d
-    xx      = x;
-    xx(j)   = x1(j);
-    f1      = FUN(xx,varargin{:});
-    if central
-      xx(j) = x0(j);
-      f0    = FUN(xx,varargin{:});
-    end
-    if j==1, J = zeros(length(f0),d); end
-    J(:,j)  = (f1-f0)/h(j);
+  switch lower(options.Vectorized)
+    case 'off'
+      %% Traditional problem
+      d         = length(x);
+      for j=1:d
+        xx      = x;
+        xx(j)   = x1(j);
+        f1      = FUN(xx,varargin{:});
+        if central
+          xx(j) = x0(j);
+          f0    = FUN(xx,varargin{:});
+        end
+        if j==1, J = zeros(length(f0),d); end
+        J(:,j)  = (f1-f0)/h(j);
+      end
+
+    case 'on'
+      %% Vectorized problem
+      d    = length(x);
+      if central, xx = x(:,ones(2*d,1));
+      else        xx = x(:,ones(d,1));
+      end
+      xx(sub2ind([d d],1:d,1:d))                 = x1;
+      if central, xx(d^2+sub2ind([d d],1:d,1:d)) = x0; end
+      ff  = FUN(xx,varargin{:});
+      if central
+        J = ff(:,1:d)-ff(:,(d+1):(2*d));
+      else
+        J = bsxfun(@minus,ff,f0);
+      end
+      J = bsxfun(@rdivide,J,h');
+
+    otherwise
+      error(['Invalid value for OPTIONS field Vectorized: must be ' ...
+             '''on'' or ''off''']);
   end
 else
   %% Serial problem
@@ -100,5 +126,6 @@ else
     J(:,:,j)  = (f1-f0)./h(:,j);
   end
   J = spblkdiag(permute(J,[2 3 1]));
+
 end
 
